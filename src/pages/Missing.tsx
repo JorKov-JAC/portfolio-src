ff177e1b1cf395cee1ffd9eafb49a20c0ad8d111
Jordan Kovacs <98484926+JorKov-JAC@users.noreply.github.com>
2023-11-23 04:44:05 -0500

    Change looks a bit

--- src/pages/Missing.tsx
+++ src/pages/Missing.tsx
@@ -1,12 +1,12 @@
 import { Component } from "solid-js"
 import { A } from "@solidjs/router"
+import * as style from "./Missing.module.css";
 
 const Missing: Component = () => {
-	
-	return <>
+	return <div class={style.style}>
 		<h1>404</h1>
 		<p>Quit snooping around, please stay on <A href="/">the trail</A>!</p>
-	</>
+	</div>
 }
 
 export default Missing
