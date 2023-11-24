7c8b535200a4775a504ca14c9872fdcb1b645d7b
Jordan Kovacs <98484926+JorKov-JAC@users.noreply.github.com>
2023-11-24 17:11:31 -0500

    Various css fixes

--- src/pages/Cv.tsx
+++ src/pages/Cv.tsx
@@ -2,6 +2,6 @@ import * as style from "/src/styles/fullpageIframe.module.css"
 
 export default function Cv() {
 	return <div class={style.style}>
-		<iframe src="/__/cvFrame.html"></iframe>
+		<iframe title="CV" src="/__/cvFrame.html"></iframe>
 	</div>
 }
